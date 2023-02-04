8e4b39ff424dd148528165818d3cdbe80a590e91
sunil parmar <sunil04ce@gmail.com>
2023-02-04 10:06:45 +0530

    completed assignment-1

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import Expenses from './components/Expenses'
+
+
+function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Xerox Paper',
+      amount: 94.12,
+      date: new Date(2023, 7, 14),
+    },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2023, 2, 12)
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2023, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2023, 5, 12),
+    }];
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Let's start</h2>
+        <Expenses items={expenses} />
+      </div>
+    </div>
+  );
+}
+
+export default App;

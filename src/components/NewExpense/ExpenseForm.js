575b516faa4e14aec2de4a569c5d37a3e103b511
sunil parmar <sunil04ce@gmail.com>
2023-02-05 20:03:57 +0530

    added sumbit handler, clear form value after submit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -49,20 +49,34 @@ const ExpenseForm = () => {
         // });
     };
 
+    const sumbitHandler = (event) => {
+        console.log('submit');
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
+
     return (
-        <form>
+        <form onSubmit={sumbitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input type="text" onChange={titleChangeHandler} value={enteredTitle} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={enteredAmount} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type="date" min="2022-01-01" max="2023-12-31" onChange={dateChangeHandler} />
+                    <input type="date" min="2022-01-01" max="2023-12-31" onChange={dateChangeHandler} value={enteredDate} />
                 </div>
             </div>
             <div className='new-expense__actions'>

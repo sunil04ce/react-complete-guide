7f36c8df51e0fdb6c56b5dbeac796d00b6c51ac5
sunil parmar <sunil04ce@gmail.com>
2023-02-06 22:14:20 +0530

    created chart based on amount with month as chartbar label

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseFilter from './ExpenseFilter';
 import ExpenseList from './ExpenseList';
+import ExpenseChart from './ExpensesChart';
 import './Expenses.css'
 
 const Expenses = (props) => {
@@ -25,6 +26,7 @@ const Expenses = (props) => {
         <>
             <Card className="expenses">
                 <ExpenseFilter selected={filteredYear} onFilter={filterHandler} />
+                <ExpenseChart expenses={filteredExpenses} />
                 <ExpenseList expenses={filteredExpenses} />
                 {/* <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
                 <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
